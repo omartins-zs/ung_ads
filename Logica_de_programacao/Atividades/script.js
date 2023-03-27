e6ce6f7351956ef66a7c9a4acb3885c6e7a3215d
omartins-zs <gabrielmatheus209@gmail.com>
2023-03-27 17:06:11 -0300

    Add Atividade Aula 2 - Calculadora de IMC

---
+++ Logica_de_programacao/Atividades/script.js
@@ -0,0 +1,6 @@
+let peso = 74; // Substitua pelo peso do usuário
+let altura = 1.87; // Substitua pela altura do usuário
+
+const imc = peso / (altura * altura);
+
+console.log(`Seu IMC é ${imc.toFixed(2)}.`);
