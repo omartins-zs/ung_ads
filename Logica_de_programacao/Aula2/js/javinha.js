5b5c66e0a1902ac98008482f485966c800d445e7
omartins-zs <gabrielmatheus209@gmail.com>
2023-03-27 16:32:39 -0300

    Declarando variaveis e Mostrando resultado no JS

---
+++ Logica_de_programacao/Aula2/js/javinha.js
@@ -0,0 +1,7 @@
+// Declaração de Variaveis
+var nome = "Gabriel Martins"
+var idade = 18
+let peso = 74
+let altura = 1.87
+
+console.log("Meu nome é " + nome + " tenho " + idade + " anos "+ "eu peso " + peso + "kg" + " e tenho " + altura +"m");
