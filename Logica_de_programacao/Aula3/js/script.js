b598de68ccc68f139b3c4fc71250c16a109319a6
omartins-zs <gabrielmatheus209@gmail.com>
2023-04-03 17:04:46 -0300

    Add Script Exemplo 3 - Boletim e Media e Aprovado

--- Logica_de_programacao/Aula3/js/script.js
+++ Logica_de_programacao/Aula3/js/script.js
@@ -18,7 +18,25 @@
 // document.write("O aluno "+ aluno + " obteve a média " + media)
 
 
+
 // Exemplo 2
+
+// var aluno = prompt("Digite o nome do aluno");
+
+// // "parseFloat" Converte String em numero float com "."
+// let nota1 = parseFloat(prompt("Digite a nota 1"));
+// let nota2 = parseFloat(prompt("Digite a nota 2"));
+// let nota3 = parseFloat(prompt("Digite a nota 3"));
+// let nota4 = parseFloat(prompt("Digite a nota 4"));
+// // Sem o parseFloat obtem media errada faz contatenação e soma de strings
+
+// var media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+// document.write("O aluno " + aluno + " tem a média " + media)
+
+
+
+// Exemplo 3
 
 var aluno = prompt("Digite o nome do aluno");
 
@@ -31,4 +49,11 @@ let nota4 = parseFloat(prompt("Digite a nota 4"));
 
 var media = (nota1 + nota2 + nota3 + nota4) / 4;
 
-document.write("O aluno " + aluno + " tem a média " + media)
+if (media >= 7) {
+    document.write("O aluno " + aluno + " foi Aprovado")
+    console.log("Aprovado");
+
+} else {
+    document.write("O aluno " + aluno + " foi Reprovado")
+    console.log("Reprovado");
+}
