2f3940e220582ec483cef391f78c0b1c1d1cf9cd
omartins-zs <gabrielmatheus209@gmail.com>
2023-04-03 16:45:34 -0300

    Add script e html da Aula 3 - Boletim e Media

---
+++ Logica_de_programacao/Aula3/js/script.js
@@ -0,0 +1,15 @@
+// Declaração de Variaveis
+// Aluno tipo STRING
+var aluno = "Gabriel Matheus";
+
+// Notas tipo INT
+let nota1 = 5
+let nota2 = 9
+let nota3 = 8
+let nota4 = 7
+let nota5 = 8
+
+var media = (nota1+nota2+nota3+nota4+nota5) / 5;
+
+console.log(media);
+document.write("O aluno "+ aluno + " obteve a média " + media)
