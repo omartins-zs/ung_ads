33553bc7a04ca8bab44c176448ddd4723a9ac4e5
omartins-zs <gabrielmatheus209@gmail.com>
2023-04-03 16:58:51 -0300

    Add Comentarios explicando o Exemplo 2

--- Logica_de_programacao/Aula3/js/script.js
+++ Logica_de_programacao/Aula3/js/script.js
@@ -22,11 +22,13 @@
 
 var aluno = prompt("Digite o nome do aluno");
 
+// "parseFloat" Converte String em numero float com "."
 let nota1 = parseFloat(prompt("Digite a nota 1"));
 let nota2 = parseFloat(prompt("Digite a nota 2"));
 let nota3 = parseFloat(prompt("Digite a nota 3"));
 let nota4 = parseFloat(prompt("Digite a nota 4"));
+// Sem o parseFloat obtem media errada faz contatenação e soma de strings
 
 var media = (nota1 + nota2 + nota3 + nota4) / 4;
 
-document.write("O aluno "+ aluno + " tem a média " + media)
+document.write("O aluno " + aluno + " tem a média " + media)
