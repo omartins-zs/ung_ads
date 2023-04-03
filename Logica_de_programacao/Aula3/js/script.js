f227ad7ad6c95ebcab35b6fe0d50d89e03364bbb
omartins-zs <gabrielmatheus209@gmail.com>
2023-04-03 16:57:42 -0300

    Add script Exemplo 2 da Aula 3 - Boletim e Media

--- Logica_de_programacao/Aula3/js/script.js
+++ Logica_de_programacao/Aula3/js/script.js
@@ -1,15 +1,32 @@
 // Declaração de Variaveis
-// Aluno tipo STRING
-var aluno = "Gabriel Matheus";
 
-// Notas tipo INT
-let nota1 = 5
-let nota2 = 9
-let nota3 = 8
-let nota4 = 7
-let nota5 = 8
+// Exemplo 1
 
-var media = (nota1+nota2+nota3+nota4+nota5) / 5;
+// // Aluno tipo STRING
+// var aluno = "Gabriel Matheus";
 
-console.log(media);
-document.write("O aluno "+ aluno + " obteve a média " + media)
+// // Notas tipo INT
+// let nota1 = 5
+// let nota2 = 9
+// let nota3 = 8
+// let nota4 = 7
+// let nota5 = 8
+
+// var media = (nota1+nota2+nota3+nota4+nota5) / 5;
+
+// console.log(media);
+// document.write("O aluno "+ aluno + " obteve a média " + media)
+
+
+// Exemplo 2
+
+var aluno = prompt("Digite o nome do aluno");
+
+let nota1 = parseFloat(prompt("Digite a nota 1"));
+let nota2 = parseFloat(prompt("Digite a nota 2"));
+let nota3 = parseFloat(prompt("Digite a nota 3"));
+let nota4 = parseFloat(prompt("Digite a nota 4"));
+
+var media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+document.write("O aluno "+ aluno + " tem a média " + media)
