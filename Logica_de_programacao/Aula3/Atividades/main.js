129e526265263b5ff460daeb76fc3aa755aa6c0b
omartins-zs <gabrielmatheus209@gmail.com>
2023-04-03 17:15:16 -0300

    Add Exemplo 2 com Loop - Logica de Par ou Impar

--- Logica_de_programacao/Aula3/Atividades/main.js
+++ Logica_de_programacao/Aula3/Atividades/main.js
@@ -1,11 +1,23 @@
 
 // Exemplo 1
 
-// Prompt -> janela - usada para inserir o numero
-let numero = prompt("Digite um numero");
+// // Prompt -> janela - usada para inserir o numero
+// let numero = prompt("Digite um numero");
 
-if (numero % 2) {
-    document.write("Numero impar")
-}else{
-    document.write("Numero par")
+// if (numero % 2) {
+//     document.write("Numero impar")
+// } else {
+//     document.write("Numero par")
+// }
+
+
+// Exemplo 2
+
+for (let i = 0; i <= 10; i++) {
+    if (i % 2) {
+        document.write("O Numero " + i + " é " + "impar" + "<br>")
+    } else {
+        document.write("Numero par" + "<br>")
+    }
+    console.log(i);
 }
