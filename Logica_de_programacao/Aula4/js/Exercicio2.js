b544458980950bc6c733729227aa8ed1e722106c
Gabriel Martins <65038297+omartins-zs@users.noreply.github.com>
2023-04-17 16:38:10 -0300

    Create Exercicio2.js

---
+++ Logica_de_programacao/Aula4/js/Exercicio2.js
@@ -0,0 +1,11 @@
+let num1 = parseInt(prompt("Digite o primeiro número: "));
+let num2 = parseInt(prompt("Digite o segundo número: "));
+let num3 = parseInt(prompt("Digite o terceiro número: "));
+let num4 = parseInt(prompt("Digite o quarto número: "));
+let num5 = parseInt(prompt("Digite o quinto número: "));
+
+let soma = num1 + num2 + num3 + num4 + num5;
+let media = soma / 5;
+
+console.log("A soma dos números é: " + soma);
+console.log("A média dos números é: " + media);
