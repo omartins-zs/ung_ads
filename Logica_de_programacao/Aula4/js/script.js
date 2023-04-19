d1140ad765391b90384a723d23420852146c5e79
omartins-zs <gabrielmatheus209@gmail.com>
2023-04-18 23:55:51 -0300

    Add Exemplo de Login e And && OR

---
+++ Logica_de_programacao/Aula4/js/script.js
@@ -0,0 +1,8 @@
+var user = prompt("Digite o usuario");
+var senha = prompt("Digite a senha");
+
+if (user == "Anselmo" && senha == "1234") {
+    alert("Acesso permitido");
+} else {
+    alert("Acesso negado");
+}
