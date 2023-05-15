95177272916e91b322a8310280fd687c11aafb78
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-15 16:05:07 -0300

    Add Melhoria do Exercicio com AND

---
+++ Logica_de_programacao/Aula6/Exercicio_1_AND.js
@@ -0,0 +1,8 @@
+var nota = prompt("Digite o usuario");
+var frequencia = prompt("Digite a senha");
+
+if (nota >= 6 && frequencia < 20) {
+    alert("Aprovado");
+} else {
+    alert("Reprovado");
+}
