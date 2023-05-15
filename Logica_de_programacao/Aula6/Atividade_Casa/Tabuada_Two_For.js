829b17fcb7495d459a27e5384206544ee203f388
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-15 15:53:11 -0300

    Fazendo Atividade Tabuada com 2 For

---
+++ Logica_de_programacao/Aula6/Atividade_Casa/Tabuada_Two_For.js
@@ -0,0 +1,5 @@
+for (var multiplicador = 1; multiplicador <= 10; multiplicador++) {
+    for (var multiplicando = 1; multiplicando <= 10; multiplicando++) {
+        console.log(multiplicador + " x " + multiplicando + " = " + (multiplicador * multiplicando));
+    }
+}
