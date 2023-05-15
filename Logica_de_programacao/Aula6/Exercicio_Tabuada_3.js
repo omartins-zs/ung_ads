04421518b5131098885b3c27e7fa6c533ab94989
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-15 15:55:07 -0300

    Add Exemplo Exercicio Tabuada do Professor

---
+++ Logica_de_programacao/Aula6/Exercicio_Tabuada_3.js
@@ -0,0 +1,9 @@
+// Exemplo professor
+
+var tabuada = prompt("Digite a tabuada")
+var ciclo = 0
+while (ciclo <= 9) {
+    document.write(tabuada + "x" + ciclo + "=" + tabuada * ciclo + "<br/>")
+    ciclo++
+}
+
