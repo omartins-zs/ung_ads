14fd8a97ea79a2ba88e6cbb8e52eeaf3e9349e35
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-15 15:58:08 -0300

    Add Exemplo Basico da Tabuada com Numero FIXO

--- Logica_de_programacao/Aula6/Exercicio_Tabuada_1.js
+++ Logica_de_programacao/Aula6/Exercicio_Tabuada_1.js
@@ -1,6 +1,7 @@
 // Exemplo basico
 
-var tabuada = prompt("Digite a Tabuada");
+// var tabuada = prompt("Digite a Tabuada");
+var tabuada = 6;
 var multiplo = 0
 while (multiplo <= 9) {
     multiplo++
