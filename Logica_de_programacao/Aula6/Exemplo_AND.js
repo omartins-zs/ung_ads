971860b066d8d689bd86b43ed142fabb70112c80
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-15 16:14:54 -0300

    Add Exemplo de AND

---
+++ Logica_de_programacao/Aula6/Exemplo_AND.js
@@ -0,0 +1,6 @@
+// Sintaxe "&&" é o operador lógico "E" ("AND" em inglês)
+// Todos precisam ser verdadeiro então retorna True ou False
+
+if (idade >= 18 && possuiCNH) {
+    console.log("Pode dirigir");
+}
