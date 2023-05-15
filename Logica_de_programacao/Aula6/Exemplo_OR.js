c6e0bf4d51e3b8a0a8c720a3ca0077ebee4f904e
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-15 16:20:25 -0300

    Add Exemplo melhorado de OR

--- Logica_de_programacao/Aula6/Exemplo_OR.js
+++ Logica_de_programacao/Aula6/Exemplo_OR.js
@@ -3,11 +3,20 @@
 // Se uma condição for verdadeira já retorna True
 
 // Verifica se o usuário tem CNH ou passaporte válido
-let temCNH = true;
-let temPassaporteValido = false;
+// let temCNH = true;
+// let temPassaporteValido = false;
 
-if (temCNH || temPassaporteValido) {
-    console.log("Acesso permitido!");
+// if (temCNH || temPassaporteValido) {
+//     console.log("Acesso permitido!");
+// } else {
+//     console.log("Acesso negado!");
+// }
+
+let idade = prompt("Qual é a sua idade?");
+let possuiCNH = confirm("Você possui CNH?");
+
+if (idade >= 18 || possuiCNH) {
+    console.log("Pode dirigir");
 } else {
-    console.log("Acesso negado!");
+    console.log("Não pode dirigir");
 }
