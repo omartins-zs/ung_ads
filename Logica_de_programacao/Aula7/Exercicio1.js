e5b7746d9230fbec9f98c4c2a7211ed2d0a1e079
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-22 10:19:23 -0300

    Renomeando e Ajustando extensoes do Arquivos

---
+++ Logica_de_programacao/Aula7/Exercicio1.js
@@ -0,0 +1,7 @@
+var n1 = prompt("Digite a nota 1");
+var n2 = prompt("Digite a nota 2");
+var n3 = prompt("Digite a nota 3");
+var n4 = prompt("Digite a nota 4");
+var media= (n1+n2+n3+n4) / 4;
+
+console.log(parseInt(media));
---
+++ Logica_de_programacao/Aula7/Exercicio4.js
@@ -0,0 +1,10 @@
+
+	let palavra = prompt("Digite uma palavra:");
+
+let novaPalavra = "";
+
+for (let i = palavra.length - 1; i >= 0; i--) {
+    novaPalavra += palavra[i];
+}
+
+document.write(novaPalavra);
