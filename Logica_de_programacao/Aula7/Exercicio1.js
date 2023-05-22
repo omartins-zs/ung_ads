c5051a7a4ef22cf116a79b52a495c9794ee89e3c
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-22 11:12:30 -0300

    Ajustando Entrada do Numero no Exercicio 1

--- Logica_de_programacao/Aula7/Exercicio1.js
+++ Logica_de_programacao/Aula7/Exercicio1.js
@@ -1,7 +1,7 @@
-var n1 = prompt("Digite a nota 1");
-var n2 = prompt("Digite a nota 2");
-var n3 = prompt("Digite a nota 3");
-var n4 = prompt("Digite a nota 4");
+var n1 = parseFloat(prompt("Digite a nota 1"));
+var n2 = parseFloat(prompt("Digite a nota 2"));
+var n3 = parseFloat(prompt("Digite a nota 3"));
+var n4 = parseFloat(prompt("Digite a nota 4"));
 var media= (n1+n2+n3+n4) / 4;
 
 console.log(parseInt(media));
