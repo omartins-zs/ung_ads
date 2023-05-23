262fa1a776dc73d617002f3dab154cae5ba19093
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-22 23:45:29 -0300

    Ajustando Style e Criando Function de Mostrar nome

--- Logica_de_programacao/Aula8/script.js
+++ Logica_de_programacao/Aula8/script.js
@@ -7,14 +7,20 @@
 //   document.getElementById("total").value = total;
 // }
 
-function valorAoQuadrado() {
-  alert("Programa para descobrir o valor Quadrado");
-  var valor = document.getElementById("number").value;
+// function valorAoQuadrado() {
+//   alert("Programa para descobrir o valor Quadrado");
+//   var valor = document.getElementById("number").value;
 
-  if (valor === "") {
-    alert("Por favor, digite um valor antes de calcular o quadrado.");
-  } else {
-    var quadrado = parseFloat(valor) * parseFloat(valor);
-    alert("O valor ao quadrado é " + quadrado);
-  }
+//   if (valor === "") {
+//     alert("Por favor, digite um valor antes de calcular o quadrado.");
+//   } else {
+//     var quadrado = parseFloat(valor) * parseFloat(valor);
+//     alert("O valor ao quadrado é " + quadrado);
+//   }
+// }
+
+function mostrarNome() {
+    nomes = ["Jorge", "Luiz", "Fernando", "Cafe", "Leo"];
+
+    document.getElementById("nome").value = nomes[2];
 }
