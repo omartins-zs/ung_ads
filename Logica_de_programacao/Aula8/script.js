939cd239bfd9e85f273f15ba15b9dfac0ccdb8ed
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-22 23:46:17 -0300

    Habilitando e Descomentando as Functions

--- Logica_de_programacao/Aula8/script.js
+++ Logica_de_programacao/Aula8/script.js
@@ -1,23 +1,23 @@
-// function exibirSoma() {
-//   n1 = document.getElementById("number").value;
-//   n2 = document.getElementById("number_two").value;
-//   total = parseInt(n1) + parseInt(n2);
+function exibirSoma() {
+    n1 = document.getElementById("number").value;
+    n2 = document.getElementById("number_two").value;
+    total = parseInt(n1) + parseInt(n2);
 
-//   alert("Irá ser feita a soma");
-//   document.getElementById("total").value = total;
-// }
+    alert("Irá ser feita a soma");
+    document.getElementById("total").value = total;
+}
 
-// function valorAoQuadrado() {
-//   alert("Programa para descobrir o valor Quadrado");
-//   var valor = document.getElementById("number").value;
+function valorAoQuadrado() {
+    alert("Programa para descobrir o valor Quadrado");
+    var valor = document.getElementById("number").value;
 
-//   if (valor === "") {
-//     alert("Por favor, digite um valor antes de calcular o quadrado.");
-//   } else {
-//     var quadrado = parseFloat(valor) * parseFloat(valor);
-//     alert("O valor ao quadrado é " + quadrado);
-//   }
-// }
+    if (valor === "") {
+        alert("Por favor, digite um valor antes de calcular o quadrado.");
+    } else {
+        var quadrado = parseFloat(valor) * parseFloat(valor);
+        alert("O valor ao quadrado é " + quadrado);
+    }
+}
 
 function mostrarNome() {
     nomes = ["Jorge", "Luiz", "Fernando", "Cafe", "Leo"];
